1b56308119d2ce634d61062daf31b16d50809356
Delowar-Hossen <delowarhossen57@gmail.com>
2022-10-04 10:49:35 +0600

    second Commit

---
+++ resources/views/layout/default.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel Multi Step Form Example </title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
+</head>
+<body style="background-color: #f5f5f5">
+      
+<div class="container">
+    <h2>Laravel Multi Step Form Example </h2>
+    @yield('content')
+</div>
+     
+</body>
+  
+</html>
